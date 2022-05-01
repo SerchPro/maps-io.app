3c8ebb273b57574406274e55614c84eb1d4e56af
SerchPro <proyects_serch@hotmail.com>
2022-04-30 19:34:49 -0500

    getting lng and lat

--- src/pages/MapaPage.js
+++ src/pages/MapaPage.js
@@ -7,13 +7,14 @@ import { useState } from 'react';
 mapboxgl.accessToken = 'pk.eyJ1IjoibWV4aWNvbGluZG84NiIsImEiOiJjbDJtaTV6cjYwZGZkM2NwN2lpNXZ5aHBwIn0.UD-1dHRAYtQXyml42YoVyQ';
 
 const initialPoint = {
-    lng:5,
-    lat:34,
-    zoom: 2
+    lng:-122.4725,
+    lat: 37.8010,
+    zoom: 13.5
 }
 export const MapaPage = () => {
     const divMap = useRef();
     const [ map, setMap] = useState();
+    const [ coords, setCoords ] = useState(initialPoint);
 
     useEffect(() => {
         const mapbx = new mapboxgl.Map({
@@ -24,10 +25,25 @@ export const MapaPage = () => {
             });
 
             setMap(mapbx)
-    }, [])
+    }, []);
+
+    useEffect(() => {
+        map?.on('move', () =>{
+            const { lng, lat} = map.getCenter();
+            setCoords({
+                lng: lng.toFixed(4),
+                lat: lat.toFixed(4),
+                zoom: map.getZoom().toFixed(2)
+            })
+        })
+    }, [map])
+    
     
     return (
         <>
+            <div className='info'>
+                Lng: { coords.lng } | lat: { coords.lat } | zoom: { coords.zoom }
+            </div>
             <div
             ref={divMap}
                 className='mapContainer'
