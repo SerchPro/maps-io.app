c3aa35a4209e36774b11331704556aaeb97482bd
SerchPro <proyects_serch@hotmail.com>
2022-05-06 23:58:06 -0500

    adding context

--- src/MapsApp.js
+++ src/MapsApp.js
@@ -1,8 +1,12 @@
 import React from 'react'
+import { SocketProvider } from './context/SocketContext'
 import { MapaPage } from './pages/MapaPage'
 
 export const MapsApp = () => {
   return (
-    <MapaPage/>
+    <SocketProvider>
+      <MapaPage/>
+    </SocketProvider>
+    
   )
 }
--- src/pages/MapaPage.js
+++ src/pages/MapaPage.js
@@ -1,5 +1,7 @@
 import React from 'react'
+import { useContext } from 'react';
 import { useEffect } from 'react';
+import { SocketContext } from '../context/SocketContext';
 import { useMapBox } from '../hooks/useMapBox'
 
 
@@ -11,18 +13,27 @@ const initialPoint = {
 export const MapaPage = () => {
 
     const { coords, setRef, newMarker$, moveMarker$ } = useMapBox(initialPoint);
+    const { socket } = useContext(SocketContext)
 
     useEffect(() => {
         newMarker$.subscribe( marker =>{
-            //console.log(marker)
+            socket.emit('new-marker', marker)
         });
-    }, [newMarker$]);
+    }, [newMarker$, socket]);
 
     useEffect(() => {
         moveMarker$.subscribe( marker =>{
             console.log(marker.id)
         });
     }, [moveMarker$]);
+
+    useEffect(() => {
+      socket.on('new-marker', (marker) =>{
+        console.log(marker)
+      });
+
+      }, [socket])
+    
     
     return (
         <>
